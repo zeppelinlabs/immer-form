1cf1770804ee7331b791ebe9f712bdb7d4a6368a
Ramiro Visca <ramiro@zeppelinlabs.io>
2021-11-19 11:40:07 -0300

    examples upgrade

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { SubmitHandler, useField, useImmerForm } from 'immer-form';
+import {
+  SubmitHandler,
+  useField,
+  useImmerFormYup,
+  useSyncErrorsWithBrowser
+} from "immer-form/Validators/YupValidator";
+import * as yup from 'yup';
 
 type FormExample = {
   name: string,
   age: number,
 }
 
-function App() {
+const useFormValidation = () => {
+  const schema: yup.SchemaOf<FormExample> = yup.object().shape({
+    name: yup.string().required(),
+    age: yup.number().required(),
+  })
+  return schema
+}
+
+const App = () => {
 
   const getDefaultValues = () => {
     return {
@@ -22,56 +36,73 @@ function App() {
       submittable,
     },
   }
-    = useImmerForm<FormExample>({
+    = useImmerFormYup<FormExample>({
       defaults: getDefaultValues(),
+      validator: useFormValidation(),
     })
 
   const {
     value: nameValue,
     onChange: onNameChange,
     onBlur: onNameBlur,
+    subToken: nameToken,
   } = useField({ token: token, attr: "name" })
 
   const {
     value: ageValue,
     onChange: onAgeChange,
     onBlur: onAgeBlur,
+    subToken: ageToken,
   } = useField({ token: token, attr: "age" })
+
+  const refName = useSyncErrorsWithBrowser<HTMLInputElement>({ token: nameToken, })
+  const refAge = useSyncErrorsWithBrowser<HTMLInputElement>({ token: ageToken, })
 
   const onSubmit: SubmitHandler<FormExample> = async (submitObject) => {
     await alert(`Submitted: ${submitObject.name} - ${submitObject.age}`)
   }
 
-  return (
-    <div className="App">
+  return (<div>
+    <div className="container">
+      <div>
+        <h1>Change me<h1 className={"exclamation"}>!</h1></h1>
+      </div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">
-          Name
+        <p>
+          <label htmlFor="name">
+            Name:
+          </label>
           <input
             id="name"
             type="text"
             value={nameValue}
             onChange={(e) => onNameChange(e.target.value)}
             onBlur={onNameBlur}
+            ref={refName}
           />
-        </label>
-        <br />
-        <label htmlFor="age">
-          Age
+
+        </p>
+        <p>
+          <label htmlFor="age">
+            Age:
+          </label>
           <input
             id="age"
-            type="number"
+            type="text"
             value={ageValue}
             onChange={(e) => onAgeChange(Number(e.target.value))}
             onBlur={onAgeBlur}
+            ref={refAge}
           />
-        </label>
+
+        </p>
         <br />
         <button
           disabled={!submittable}
           type="submit">Submit</button>
       </form>
     </div>
+  </div>
   );
 }
 
